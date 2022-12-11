d34ad89b89ee1c31a80439bc2b1836a54d0b91e1
LiudmylaSheva <111154331+LiudmylaSheva@users.noreply.github.com>
2022-12-12 01:14:10 +0200

    Made main style for app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Weather from "./Weather";
 function App() {
 	return (
 		<div className="App container">
-			<h1>Hello World</h1>
+			<h1 className="title">
+				The weather application for your daily routine{" "}
+				<i class="fa-solid fa-hurricane fa-spin"></i>
+			</h1>
 			<Weather />
+			<footer>
+				{" "}
+				Created by Shevadutska Liudmyla and this project is{" "}
+				<a href="https://github.com/LiudmylaSheva/react-weather-app">
+					open-sourced
+				</a>{" "}
+			</footer>
 		</div>
 	);
 }
--- src/Weather.js
+++ src/Weather.js
@@ -6,26 +6,33 @@ export default function Weather() {
 		<div className="Weather">
 			<form>
 				<div className="row">
-					<div className="col-9">
+					<div className="col-8 search">
 						<input
 							type="search"
 							placeholder="Search city..."
 							className="form-control"
+							autoFocus="on"
 						/>{" "}
 					</div>
-					<div className="col-3">
-						<input type="submit" value="Search" className="btn  btn-primary" />
+					<div className="col-4 search-b">
+						<input
+							type="submit"
+							value="Search"
+							className="btn  btn-primary w-50"
+						/>
 					</div>
 				</div>
 			</form>
-			<h1>Lisbon</h1>
-			<ul>
-				<li>Saturday, 20:21</li>
-				<li>Mostly Cloudy</li>
-			</ul>
+			<h1 className="city">Lisbon</h1>
+			<div className="row main-info">
+				<div className="col-6">Mostly Cloudy</div>
+				<div className="col-6">Saturday, 20:21</div>
+			</div>
 			<div className="row">
 				<div className="col-6">
-					<img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" /> 6°C
+					<img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" />{" "}
+					<span className="temperature"> 6</span>
+					<span className="unit">°C</span>
 				</div>
 				<div className="col-6">
 					<ul>
